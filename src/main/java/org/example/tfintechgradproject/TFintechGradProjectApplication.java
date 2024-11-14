746cc0fa976cbe4c3bddc455660a70edad85208c
vladimir-arsenov <arsenovova7@gmail.com>
2024-11-14 23:55:38 +0300

    Initial commit

---
+++ src/main/java/org/example/tfintechgradproject/TFintechGradProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.tfintechgradproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TFintechGradProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TFintechGradProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/tfintechgradproject/TFintechGradProjectApplicationTests.java
@@ -0,0 +1,14 @@
+package org.example.tfintechgradproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TFintechGradProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
