db9336c0f8de0a7f41effe1811e4ae8afb3f7d0d
vladimir-arsenov <arsenovova7@gmail.com>
2024-12-06 06:27:07 +0300

    add validation exception handler

--- src/main/java/org/example/tfintechgradproject/security/dto/AuthenticationRequestDto.java
+++ src/main/java/org/example/tfintechgradproject/security/dto/AuthenticationRequestDto.java
@@ -1,5 +1,7 @@
 package org.example.tfintechgradproject.security.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,7 +13,12 @@ import lombok.NoArgsConstructor;
 @Builder
 public class AuthenticationRequestDto {
 
+  @Email(message = "Invalid email format")
+  @NotBlank(message = "email cannot be blank")
   private String email;
+
+  @NotBlank(message = "password cannot be blank")
   private String password;
+
   private boolean rememberMe;
 }
